a8eb85a5fc87a410eadbc6dba4564de71169b60a
maxiaoqiang <maxiaoqiang@arxanchain.com>
2020-02-26 12:09:58 +0800

    [ID1000320] create organization structure card component
    --story=1000320 --user=mxqiang
    https://www.tapd.cn/60210943/prong/stories/view/1160210943001000320
    
    Signed-off-by: maxiaoqiang <maxiaoqiang@arxanchain.com>

---
+++ src/components/card/organ.vue
@@ -0,0 +1,39 @@
+<template>
+   <div v-if="type === 'icon-left'">
+     <organCardLeft :info="info" />
+   </div>
+   <div v-else-if="type === 'icon-center'">
+     <organCardCenter :info="info"/>
+   </div>
+   <div v-else>
+     <organCard :info="info"/>
+   </div>
+</template>
+
+<script>
+import organCard from './module/organCard'
+import organCardLeft from './module/organCardLeft'
+import organCardCenter from './module/organCardCenter'
+export default {
+  components: {
+    organCardCenter,
+    organCardLeft,
+    organCard
+  },
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    info: {
+      type: Object,
+      default: {}
+    }
+  },
+  onLoad () {
+    console.log('-------')
+    console.log(this.info)
+  }
+}
+</script>
+
