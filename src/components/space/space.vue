43525ae137b02aef449a633daae33e87d091149a
littleSky <mobai0419@users.noreply.github.com>
2020-02-28 09:59:25 +0800

    [ID1000324] create cell list component
        --story=1000324 --user=mxqiang
        https://www.tapd.cn/60210943/prong/stories/view/1160210943001000324
    
        Signed-off-by: maxiaoqiang <maxiaoqiang@arxanchain.com>

---
+++ src/components/space/space.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="space-container" :style="{ 'height': height + 'rpx' }">
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    height: String,
+    default: '16'
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/style/common.scss";
+.space-container{
+  background: $background-F3F3F3;
+  height: 16rpx;
+}
+</style>
+
