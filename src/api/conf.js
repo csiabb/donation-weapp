30a63f25f639d05c3d441c224d0929bc8329222e
littleSky <mobai0419@users.noreply.github.com>
2020-03-02 11:51:54 +0800

    [ID1000330] add base request
        --story=1000330 --user=mxqiang
        https://www.tapd.cn/60210943/prong/stories/view/1160210943001000330
    
        Signed-off-by: maxiaoqiang <maxiaoqiang@arxanchain.com>

---
+++ src/api/conf.js
@@ -0,0 +1,11 @@
+/*
+Copyright ArxanChain Ltd. 2020 All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+const basic = 'https://boxdev.arxanchain.com'
+
+export default {
+  donation: basic
+}
---
+++ src/api/home.js
@@ -0,0 +1,11 @@
+/*
+Copyright ArxanChain Ltd. 2020 All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+import { get } from '@/api/index'
+
+export function pubList (params) {
+  return get('/pub/list', params)
+}
